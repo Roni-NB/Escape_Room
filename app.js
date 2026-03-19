df5c01d514f5ddbb5dab234950aea7732bdd86f0
Roni-NB <deedee.gazit@gmail.com>
2026-03-19 12:41:05 +0100

    Created databaseschema, deployed, and made the email part in the computer

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+import 'dotenv/config'
 const express = require('express');
 const app = express();
 app.set('view engine', 'ejs')
 const path = require('path');
 const ejs = require('ejs');
-const port = 3000;
 app.use (express.static (__dirname))
+mongoose.connect(process.env.MONGODB_URI)
 
 app.get('/', (request, response) => {
   response.render('library_entrance');
@@ -20,6 +21,10 @@ app.get('/howtoplay', (request, response) => {
 
 app.get('/hauntedlibrary', (request, response) => {
       response.render('inside_the_library');
+});
+
+app.get('/insidecomputer', (request, response) => {
+      response.render('inside_computer');
 });
 
 app.get('/hauntedlibrary/:clues', (request, response) => {
--- schema.js
+++ schema.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 const emailSchema = new mongoose.Schema({
-  subject: String,
   email: String,
+  subject: String,
   content: String, 
   date: Boolean
 })
 export const email = mongoose.model('Email', emailSchema)
+
